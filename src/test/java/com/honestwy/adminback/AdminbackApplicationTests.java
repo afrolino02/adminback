5474aaf87df417994562b5572d9eba563a5d4217
qwerjesus <pythonadsi@gmail.com>
2025-12-15 15:22:40 -0500

    adminInitial

---
+++ src/test/java/com/honestwy/adminback/AdminbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honestwy.adminback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
